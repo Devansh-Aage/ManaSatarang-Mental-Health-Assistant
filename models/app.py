7d64a0bad7659ed5e5edbfa56f893ebdb6108b1c
AakashHubGit <aakash.pandey@somaiya.edu>
2024-07-27 15:58:33 +0530

    UPDATED Models

--- models/app.py
+++ models/app.py
@@ -3,6 +3,7 @@ import torch
 import tensorflow as tf
 from flask import Flask, request, jsonify,render_template
 from PIL import Image
+from flask_cors import CORS
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from ultralytics import YOLO
@@ -10,6 +11,7 @@ from ultralytics import YOLO
 # names: {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
 # Initialize Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Load the trained VGG16 model
 classifier_model = tf.keras.models.load_model('multi_activity_classifier_vgg16.h5')
@@ -29,6 +31,7 @@ def index():
 # Define a route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
+    print(request.files)
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
 
@@ -38,7 +41,7 @@ def predict():
     # Get the image from the request
     image_file = request.files['image']
     activity = request.form['activity'].strip().lower()
-
+    
     if activity not in valid_activities:
         return jsonify({'error': f'Invalid activity. Valid activities are {valid_activities}'}), 400
 
@@ -99,4 +102,4 @@ def predict():
 if __name__ == '__main__':
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    app.run(debug=True,port=5001)
+    app.run(debug=True,port=5000)
